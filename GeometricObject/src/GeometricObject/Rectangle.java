2872291478fd62a8341177fb3b58a0a79ed3e933
nisa-thk <nisa.ozturk365@gmail.com>
2025-12-21 18:42:00 +0300

    Add files via upload

---
+++ GeometricObject/src/GeometricObject/Rectangle.java
@@ -0,0 +1,46 @@
+package GeometricObject;
+
+public class Rectangle extends GeometricObject {
+	  private double height;​
+	    private double width;​
+	​
+	    public Rectangle(){​
+	    }​
+	​
+	    public Rectangle(double height, double width){​
+	        this.height = height;​
+	        this.width = width;​
+	    }​
+	​
+	    public Rectangle(double height, double width, String color, boolean filled){​
+	        this.height = height;​
+	        this.width = width;​
+	        setColor(color);​
+	        setFilled(filled);​
+	    }​
+	​
+	    public double getHeight(){​
+	        return height;​
+	    }​
+	    public double getWidth(){​
+	        return width;​
+	    }​
+	​
+	    public void setHeight(double height){​
+	        this.height = height;​
+	    }​
+	​
+	    public void setWidth(double width){​
+	        this.width = width;​
+	    }​
+	​
+	    public double getArea(){​
+	        return height* width;​
+	    }​
+	​
+	    public double getPerimeter(){​
+	        return 2 * (height + width);​
+	    }​
+	}
+
+}
