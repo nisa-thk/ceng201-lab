c7db8ee1776fa137979e39576ec724b7c8fafc3a
nisa-thk <nisa.ozturk365@gmail.com>
2025-12-21 19:42:03 +0300

    Refactor TreatmentRequest class with corrections
    Fixed import statement and corrected arrivalTime assignment. Updated toString method to use Date object for arrival time.

--- Ceng201HW_Assigments/src/Ceng201HW_Assigments/TreatmentRequest.java
+++ Ceng201HW_Assigments/src/Ceng201HW_Assigments/TreatmentRequest.java
@@ -1,16 +1,18 @@
 package Ceng201HW_Assigments;
-
+impoert java.util.Date;
 public class TreatmentRequest {
 	 int patientId;
 	 long arrivalTime;
 	 boolean priority; //For determine the priority
 	 public TreatmentRequest(int patientId, long arrivalTime, boolean priority) {
 	    this.patientId = patientId;
-	    this.arrivalTime = System.currentTimeMillis();
+	    this.arrivalTime = arrivalTime;
 	    this.priority = priority;
 	}
 	@Override
 	public String toString(){
-	    return "Request{id: " + patientId + ", arrival time: " +", priority: " + priority + "}";
+		Date date = new Date(arrivalTime);
+	    return "Request{id: " + patientId + ", arrival time: " + date +", priority: " + priority + "}";
 	 }
 }
+
